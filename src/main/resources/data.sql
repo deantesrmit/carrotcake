c98c2900156953bb89fd4da4667431e57ba7a182
Rhys <s3611475@student.rmit.edu.au>
2018-05-08 18:16:39 +1000

    slideshow and form alignment fixed
    SQL files
    - added 'available' (bit/bool) attribute to room table

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,8 @@
 insert into room
-values(10001,'Room1', 'gold', 1 );
+values(10001,'Room1', 'gold', 1, 1 );
 insert into room
-values(10002,'Room2', 'platinum', 2 );
+values(10002,'Room2', 'platinum', 2, 1 );
 insert into room
-values(10003,'Room3', 'diamond', 2);
+values(10003,'Room3', 'diamond', 2, 1);
 insert into room
-values(10004,'Room4', 'californium', 4);
+values(10004,'Room4', 'californium', 4, 1);
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -4,5 +4,6 @@ CREATE TABLE room (
    id INT NOT NULL,
    room_number VARCHAR(50) NOT NULL,
    room_type VARCHAR(50) NOT NULL,
-   num_rooms INT NOT NULL
+   num_rooms INT NOT NULL,
+   available BIT NOT NULL
 );
